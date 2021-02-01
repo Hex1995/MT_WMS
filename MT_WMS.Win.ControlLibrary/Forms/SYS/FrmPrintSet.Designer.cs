7994731208ddda817f43d9de391cb057c296ac9c
旭阳 何 <522707831@qq.com>
2021-02-01 16:15:55 +0800

    同步之前的代码

--- MT_WMS.Win.ControlLibrary/Forms/SYS/FrmPrintSet.Designer.cs
+++ MT_WMS.Win.ControlLibrary/Forms/SYS/FrmPrintSet.Designer.cs
@@ -30,19 +30,72 @@ namespace MT_WMS.Win.ControlLibrary.Forms.SYS
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.TrvPrintList = new System.Windows.Forms.TreeView();
+            this.BtnPrintRefresh = new FontAwesome.Sharp.IconButton();
+            this.BtnPrintSave = new FontAwesome.Sharp.IconButton();
+            this.TxtPrintNow = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.TrvPrintList = new System.Windows.Forms.TreeView();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.BtnPrintRefresh);
+            this.panel1.Controls.Add(this.BtnPrintSave);
+            this.panel1.Controls.Add(this.TxtPrintNow);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(3, 26);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(794, 58);
             this.panel1.TabIndex = 0;
+            // 
+            // BtnPrintRefresh
+            // 
+            this.BtnPrintRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnPrintRefresh.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.BtnPrintRefresh.IconColor = System.Drawing.Color.Black;
+            this.BtnPrintRefresh.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BtnPrintRefresh.Location = new System.Drawing.Point(584, 13);
+            this.BtnPrintRefresh.Name = "BtnPrintRefresh";
+            this.BtnPrintRefresh.Size = new System.Drawing.Size(110, 31);
+            this.BtnPrintRefresh.TabIndex = 3;
+            this.BtnPrintRefresh.Text = "刷新打印机列表";
+            this.BtnPrintRefresh.UseVisualStyleBackColor = true;
+            this.BtnPrintRefresh.Click += new System.EventHandler(this.BtnPrintRefresh_Click);
+            // 
+            // BtnPrintSave
+            // 
+            this.BtnPrintSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnPrintSave.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.BtnPrintSave.IconColor = System.Drawing.Color.Black;
+            this.BtnPrintSave.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BtnPrintSave.Location = new System.Drawing.Point(458, 13);
+            this.BtnPrintSave.Name = "BtnPrintSave";
+            this.BtnPrintSave.Size = new System.Drawing.Size(110, 31);
+            this.BtnPrintSave.TabIndex = 2;
+            this.BtnPrintSave.Text = "设置当前打印机";
+            this.BtnPrintSave.UseVisualStyleBackColor = true;
+            // 
+            // TxtPrintNow
+            // 
+            this.TxtPrintNow.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.TxtPrintNow.Location = new System.Drawing.Point(147, 13);
+            this.TxtPrintNow.Name = "TxtPrintNow";
+            this.TxtPrintNow.ReadOnly = true;
+            this.TxtPrintNow.Size = new System.Drawing.Size(305, 31);
+            this.TxtPrintNow.TabIndex = 1;
+            this.TxtPrintNow.Text = "Microsoft Print to PDF";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "当前打印机：";
             // 
             // TrvPrintList
             // 
@@ -52,15 +105,6 @@ namespace MT_WMS.Win.ControlLibrary.Forms.SYS
             this.TrvPrintList.Name = "TrvPrintList";
             this.TrvPrintList.Size = new System.Drawing.Size(794, 313);
             this.TrvPrintList.TabIndex = 9;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 31);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 12);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
             // 
             // FrmPrintSet
             // 
@@ -82,5 +126,8 @@ namespace MT_WMS.Win.ControlLibrary.Forms.SYS
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TreeView TrvPrintList;
+        private FontAwesome.Sharp.IconButton BtnPrintSave;
+        private System.Windows.Forms.TextBox TxtPrintNow;
+        private FontAwesome.Sharp.IconButton BtnPrintRefresh;
     }
 }
--- MT_WMS.Win.ControlLibrary/Forms/SYS/FrmPrintSet.cs
+++ MT_WMS.Win.ControlLibrary/Forms/SYS/FrmPrintSet.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,23 @@ namespace MT_WMS.Win.ControlLibrary.Forms.SYS
         public FrmPrintSet()
         {
             InitializeComponent();
+            InitialPrint();
+        }
+        void InitialPrint()
+        {
+            List<TreeNode> printList = new List<TreeNode>();
+            foreach (var item in PrinterSettings.InstalledPrinters)
+            {
+                printList.Add(new TreeNode() { Name = item.ToString(), Text = item.ToString(), Tag = item.ToString() });
+            }
+            TrvPrintList.Nodes.Clear();
+            TrvPrintList.Nodes.AddRange(printList.ToArray());
+            TxtPrintNow.Text = ConfigHelper.GetKeyValue("PrintName");
+        }
+
+        private void BtnPrintRefresh_Click(object sender, EventArgs e)
+        {
+            InitialPrint();
         }
     }
 }
